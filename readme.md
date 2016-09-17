1cb1ec88cd1374e70d6935728137ef2b6198c5b9
Evgeniy Agasyanc <agasyanc@gmail.com>
2016-09-17 22:40:10 +0400

    fix last element and add my first readme

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Riser jquery plugin
+It my first and draft plugin. Riser waits for images in elements with `.rise` class and then rise up them.
+
+Rise and shine!
--- rise.js
+++ rise.js
@@ -1,52 +1,57 @@
-//Rise 0.2 Say Hi!
+//Rise 0.3
+//hide all rise-element by opacity
 document.write("<style>.rise{opacity:0}</style>");
-var speed = 500
+var speed = 500;
+var false_time = 1000;
+
 $(function(){
-  var rises = $(".rise");
+
+  window.setTimeout(show_on_false, false_time);
+  function show_on_false() {
+    rise($(".rise"));
+  }
+
+  var rises = [];
   var urls = [];
   var counts = [];
 
   var newImages = [];
 
-  rises.each(function() {
+  $(".rise").each(function() {
     var tmp = [];
     var images = $(this).find('img');
-    images.each(function(index, el){
-      tmp.push(el.src);
-    });
-    urls.push(tmp);
-    counts.push(0);
-
-    var loaders = [];
-
+    if (images.length) {
+      rises.push(this);
+      images.each(function(index, el){
+        tmp.push(el.src);
+      });
+      urls.push(tmp)
+      counts.push(images.length);
+    }
+    else{
+      //if no images in rise-element
+      rise(this);
+    }
   });
 
   function imageLoad (index, el) {
-    counts[index]++;
-
-        if (counts[index] == el.length) {
-          setTimeout(function() {
-            rise(rises[index])
-          }, 200);
-        }
+    counts[index]--;
+    if (counts[index]==0){
+      rise(rises[index]);
+    }
   }
-
-  $.each(urls, function(index, el) {
-    $.each(el, function(k, url) {
+  $.each(urls, function(i, element) {
+    $.each(element, function(k, el) {
       newImage = new Image();
-      newImage.onLoad=imageLoad(index, el);
+      newImage.onLoad=imageLoad(i, el);
       newImage.scr = el;
       newImages.push(newImage);
     });
+  });
+
+});
+function rise(object) {
+    $(object).animate({'opacity': 1}, speed, function() {
 
     });
-
-  function rise (object) {
-      $(object).animate({
-        'opacity': 1,
-          },
-        speed, function() {
-
-      });
-  }
-})
+}
